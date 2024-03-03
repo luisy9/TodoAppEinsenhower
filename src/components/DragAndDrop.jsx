41ca35843fd20fb1aff5e891b459d5d251d39f7f
Luis de Haro <luisdeharo4@gmail.com>
2024-03-03 12:27:34 +0100

    add card +, close task

--- src/components/DragAndDrop.jsx
+++ src/components/DragAndDrop.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Item } from './';
 
 export const DragAndDrop = () => {
-    const [addTask, setAddTask] = useState({});
+    const [addTask, setAddTask] = useState([]);
+    //Tenemos que diferenciar cual es el ultimo elemento escrito en el textarea
+    const [newTask, setNewTask] = useState([]);
+
     const [items, setItems] = useState([
         {
             id: 10,
@@ -58,6 +61,10 @@ export const DragAndDrop = () => {
         }
     ])
 
+    // useEffect(() => {
+    //     closeTaskClick(addTask);
+    // }, [items])
+
     const onDragItem = (event, id, category) => {
         event.dataTransfer.setData('itemID', id);
     }
@@ -78,8 +85,23 @@ export const DragAndDrop = () => {
         // localStorage.setItem('todos', JSON.stringify(items));
     }
 
+    //Habrimos el div para añadir una tarea
     const addTaskClick = (id) => {
-        // setAddTask(task => {...task} { id: { id: true } });
+        setAddTask(task => [...task, { id: { [id]: true } }]);
+    }
+
+    //Close el Div para añadir una tarea
+    const closeTaskClick = (id) => {
+        console.log(id)
+        setAddTask(task => task.filter(t => t.id !== id))
+    }
+
+    //Añadimos una tarea al state, y tenemes que diferenciar bien que textarea se tiene que cerrar
+    const addTaskState = (id) => {
+        const newTaskAdd = newTask;
+        setItems(items => [...items, { id: items[items.length - 1].id + 1, name: newTaskAdd, category: id }]);
+        const theTaskToClose = addTask.filter(e => e.id !== id)[0].id;
+        // closeTaskClick(theTaskToClose);
     }
 
 
@@ -95,10 +117,36 @@ export const DragAndDrop = () => {
                                 items.map(item => item.category === box.id ?
                                     <Item key={item.id} items={item} box={box} onDragItem={onDragItem} /> : <></>)
                             }
-                            {box.name != 'delete' ? <button className="text-gray-500 pt-1 text-sm hover:text-white hover:duration-300" onClick={() => addTaskClick(box.id)}>Add card +</button> : <></>}
+                            {box.name != 'delete' ? (
+                                <div className="flex justify-start">
+                                    <div className="">
+                                        <button className="text-gray-500 pt-3 px-2 text-sm font-medium hover:text-white hover:duration-300" onClick={() => addTaskClick(box.id)}>Add card +</button>
+                                    </div>
+                                </div>
+
+                            ) : <></>}
                             {
-                                
-                                // boxes.filter(box => box.id) ? <div className=""><h1>Hola</h1></div> : <></>
+
+                                addTask.map(e => {
+                                    if (Object.keys(e.id) == box.id) {
+                                        return (
+                                            <>
+                                                <div className="">
+                                                    <textarea onChange={() => setNewTask(event.target.value)} className="w-56 bg-[#322F44] h-20 border rounded-md border-[#A78BFA] 
+                                                focus:ring-1 focus:outline-none focus:ring-[#A78BFA] placeholder:text-[#C5A4C9] px-3 py-3" placeholder="Add new task..."></textarea>
+                                                    <div className="flex justify-end items-center gap-5 py-1">
+                                                        <div className="">
+                                                            <button className="text-[#A39E9E] text-sm hover:text-white hover:duration-300" onClick={() => closeTaskClick(e.id)}>Close</button>
+                                                        </div>
+                                                        <div className="">
+                                                            <button className="bg-white px-3 py-1 border-none rounded-md text-sm hover:bg-slate-300" onClick={() => addTaskState(box.id)}>Add +</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )
+                                    }
+                                })
                             }
                         </div>
                     )
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,7 @@ export const Item = ({ items, box, onDragItem }) => {
 
   return (
     <div>
-      <div className="cursor-grab border-[#404040] border rounded-lg px-1 py-2 mt-5 bg-[#272627] w-56"
+      <div className="cursor-grab border-[#404040] border rounded-lg px-3 py-2 mt-5 bg-[#272627] font-medium w-56"
         onDragStart={() => onDragItem(event, items.id, items.category)}>
         <p draggable className="text-white">{items.name}</p>
       </div>
